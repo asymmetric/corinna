198b26f1d69dee9258be04872d1f577a5049f740
Carmine Paolino <earcar@gmail.com>
2009-07-05 17:45:40 +0200

    find() implemented and works
    find(:one) is not necessary, can we have to disable it?

--- app/models/treasure_hunt.rb
+++ app/models/treasure_hunt.rb
@@ -1,95 +1,64 @@
-class TreasureHunt
-  include HTTParty
-  base_uri 'localhost:3000'
-  format :xml
-  headers 'Accept' => 'text/xml'
-
+class ActiveTreasureHunt < ActiveResource::Base
   class << self
-    def create(hash)
-      # load
-      # fetch id from xml
-      self.new(hash) # done :)
-    end
-
-    def find(*arguments)
-      scope   = arguments.slice!(0)
-      options = arguments.slice!(0) || {}
-
-      case scope
-      when :all   then find_every(options)
-      when :first then find_every(options).first
-      when :last  then find_every(options).last
-      else             find_single(scope, options)
-      end
-    end
-
-    def delete(id)
-      # remove
-    end
-
-    def exists?(id)
-    end
+#    Dunno if we need that method..
+#    I have only removed the ":one" option which doesn't makes sense for us
+#    def find(*arguments)
+#      scope   = arguments.slice!(0)
+#      options = arguments.slice!(0) || {}
+#
+#      case scope
+#      when :all   then find_every(options)
+#      when :first then find_every(options).first
+#      when :last  then find_every(options).last
+#      else             find_single(scope, options)
+#      end
+#    end
 
     private
+    # Find every resource
+    # The same but connection.get()[element_name] to get the right object
     def find_every(options)
-      instantiate_collection(fetch_collection)
+      case from = options[:from]
+      when Symbol
+        instantiate_collection(get(from, options[:params]))
+      when String
+        path = "#{from}#{query_string(options[:params])}"
+        instantiate_collection(connection.get(path, headers)[element_name] || [])
+      else
+        prefix_options, query_options = split_options(options[:params])
+        path = collection_path(prefix_options, query_options)
+        instantiate_collection((connection.get(path, headers)[element_name] || []), prefix_options )
+      end
     end
 
+    # Find a single resource from the default URL
     def find_single(scope, options)
-      unless scope.is_a? String
-        scope = scope.to_s
-      end
-
-      fetch_collection.each do |object|
+      scope = scope.to_s unless scope.is_a? String
+      prefix_options, query_options = split_options(options[:params])
+      path = collection_path(prefix_options, query_options)
+      connection.get(path, headers)[element_name].each do |object|
         return instantiate_record(object) if object['id'] == scope
       end
-      nil # ugly
-    end
-    
-    def fetch_collection
-      get(collection_path)['thunt:getTreasureHunts']['thunt:treasureHunt'] # TODO ugly ugly ugly!
+      nil
     end
 
-    attr_accessor :collection_name
-    def collection_path
-      "/#{collection_name}" # TODO prefix, prefix_path, prefix_options
+    def collection_path(prefix_options = {}, query_options = nil)
+      prefix_options, query_options = split_options(prefix_options) if query_options.nil?
+      "#{prefix(prefix_options)}#{collection_name}#{query_string(query_options)}"
     end
-    def instantiate_collection(collection)
+
+    def instantiate_collection(collection, prefix_options = {})
       unless collection.kind_of? Array
-        [instantiate_record(collection)]
+        [instantiate_record(collection, prefix_options)]
       else
-        collection.collect! { |record| instantiate_record(record) }
+        collection.collect! { |record| instantiate_record(record, prefix_options) }
       end
     end
-    def instantiate_record(record)
-      new(record)
-    end
   end
+
+end
+
+class TreasureHunt < ActiveTreasureHunt
+  self.site = 'http://localhost:3000'
   self.collection_name = 'gettreasurehunts'
-
-  attr_reader :id, :title, :description
-  def initialize(hash)
-    @id = hash['id']
-    @title = hash['title']
-    @description = hash['thunt:description']
-  end
-
-  def new?
-    id.nil? # doesn't work anymore
-  end
-
-  def save
-    new? ? create : update
-  end
-  
-  def destroy
-  end
-
-  protected
-
-  def create
-  end
-
-  def update
-  end
 end
