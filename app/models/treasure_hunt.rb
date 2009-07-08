fce7894cfd84c04da9ccf7a22e5a3900b7ba3868
Carmine Paolino <earcar@gmail.com>
2009-07-08 13:28:44 +0200

    Sorry, I've changed the self.site

--- app/models/treasure_hunt.rb
+++ app/models/treasure_hunt.rb
@@ -1,5 +1,5 @@
 class TreasureHunt < ActiveTreasureHunt::Base
-  self.site = 'http://localhost:3001'
+  self.site = 'http://xanadu.doesntexist.com/stanis'
   self.collection_name = 'gettreasurehunts'
   self.create_name = 'loadtreasurehunt'
   self.headers = { "Accept" => "text/xml" , "Content-Type" => "application/x-www-form-urlencoded" }
