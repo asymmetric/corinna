9619e8798c71d248ee8204c00789f689a2a0214b
Alejanmat <alejanmat@hotmail.it>
2009-07-11 16:03:41 +0200

    Modifiche alla create

--- app/models/treasure_hunt.rb
+++ app/models/treasure_hunt.rb
@@ -1,7 +1,8 @@
 class TreasureHunt < ActiveTreasureHunt::Base
-  self.site = 'http://xanadu.doesntexist.com/stanis'
-  #self.site = 'http://localhost:3001'
+  #self.site = 'http://xanadu.doesntexist.com/stanis'
+  self.site = 'http://localhost:3001'
   self.collection_name = 'gettreasurehunts'
   self.create_name = 'loadtreasurehunt'
-  self.headers = { "Content-Type" => "application/x-www-form-urlencoded" }
+  self.remove_name = 'removetreasurehunt'
+  self.headers = { "Accept" => "text/xml", "Content-Type" => "application/x-www-form-urlencoded" }
 end
