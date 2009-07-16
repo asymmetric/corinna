da56f44fcdc9861c7fca615db3e3fe2cab814109
asymmetric <asymmetric@autistici.org>
2009-07-16 12:23:26 +0200

    anche quando ti iscrivi come gruppo, ti iscrivi come user

--- app/controllers/treasure_hunts_controller.rb
+++ app/controllers/treasure_hunts_controller.rb
@@ -72,8 +72,10 @@ class TreasureHuntsController < ApplicationController
         flash[:notice] = "Successfully subscribed to Treasure Hunt #{@hunt.id}"
       when "group"
         group_id = params[:group_id]
-        @hunt.subscribe :group, group_id, @current_user.password
+        @hunt.subscribe :group, @current_user.id, @current_user.password
+        #@hunt.subscribe :group, group_id, @current_user.password
         @current_user.subscriptions << { :id => @hunt.id } # TODO la aggiungiamo in questo caso?
+        @current_user.save
         flash[:notice] = "Successfully subscribed to hunt #{@hunt.id} as group #{group_id}"
       end
     rescue => e
