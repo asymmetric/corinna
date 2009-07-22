2b0f15aa3b0d55f17a6d1940ac7d246dbc810e4b
Carmine Paolino <earcar@gmail.com>
2009-07-22 14:43:24 +0200

    maps

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -4,8 +4,7 @@ class MapsController < ApplicationController
     @map = GMap.new("map_div")
     @map.control_init(:large_map => true,:map_type => true)
     @map.center_zoom_init([params[:lat].to_f,params[:lng].to_f],params[:z].to_i)
-    @map.overlay_init(GMarker.new([params[:lat].to_f,params[:lng].to_f],:title => "What are you staring at?", :info_window => "Info! Info!"))
-    @map.overlay_init(GMarker.new([params[:lat].to_f + 0.1,params[:lng].to_f + 0.1],:title => "What are yo staring at?", :info_window => "Info1! Info1!"))
+    @map.overlay_init(GMarker.new([params[:lat].to_f,params[:lng].to_f],:title => "What are you staring at?", :info_window => "Hi! :-)"))
     respond_to do |format|
       format.html
     end
