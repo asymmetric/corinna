c33cdbd1dbc1a0318a52c664f9aab31e59a184ab
asymmetric <asymmetric@autistici.org>
2009-07-22 12:23:19 +0200

    fixata risposta URI

--- app/controllers/treasure_hunts_controller.rb
+++ app/controllers/treasure_hunts_controller.rb
@@ -179,7 +179,7 @@ class TreasureHuntsController < ApplicationController
 
   def answer
 
-   	# shows form to input the answer
+    # shows form to input the answer
     if request.get?
       @hunt = TreasureHunt.find params[:id]
 
@@ -192,15 +192,15 @@ class TreasureHuntsController < ApplicationController
 
     elsif request.post?
       @hunt = TreasureHunt.find params[:id]
-      @answer = params[:answer]
-      @answer_type = params[:answer_type].downcase.intern
-			if @answer_type == :geoloc
-				cane = params[:title]
-				validates_numericality_of :cane
-				@answer[:lat] = params[:geoloc_lat]
-				@answer[:long] = params[:geoloc_long]
-				@answer[:planet] = params[:geoloc_planet]
-			end
+      @answer_type = params[:answer_type]
+      if @answer_type == :geoloc
+        @answer = {}
+        @answer[:lat] = params[:geoloc_lat]
+        @answer[:long] = params[:geoloc_long]
+        @answer[:planet] = params[:geoloc_planet]
+      else
+        @answer = params[:answer]
+      end
 
       begin
         @resp = @hunt.answer @answer, @answer_type, @current_user.id, @current_user.password
