c019a599263bf40de7812f4e2b3272a2469abbc0
Carmine Paolino <earcar@gmail.com>
2009-09-01 14:13:46 +0200

    trying to fix win bug

--- app/controllers/treasure_hunts_controller.rb
+++ app/controllers/treasure_hunts_controller.rb
@@ -79,7 +79,7 @@ class TreasureHuntsController < ApplicationController
         end
         format.fbml
       rescue Exception => e
-        flash[:error] = e.message unless flash[:info]
+        flash[:error] = e.message unless flash[:info] or flash[:notice]
         format.fbml { redirect_to [@server, @hunt] }
       end
     end
