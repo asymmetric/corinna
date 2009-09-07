7ec557328cf2b1cf5a1e44f7ca1fb646a6bebdd0
Carmine Paolino <earcar@gmail.com>
2009-09-07 19:05:29 +0200

    Removed non used gems

--- config/environment.rb
+++ config/environment.rb
@@ -14,11 +14,9 @@ Rails::Initializer.run do |config|
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
   # Specify gems that this application depends on and have them installed with rake gems:install
-  config.gem "facebooker"
   config.gem "nokogiri"
   config.gem "geokit"
   config.gem "flickraw"
-  config.gem "bluecloth"
   # config.gem "libxml-ruby", :lib => "libxml"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
