8c5e53eb734db2cc09ffc05c9618752e6a6b8c8b
Carmine Paolino <earcar@gmail.com>
2009-09-09 23:00:03 +0200

    Done!!

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -4,14 +4,14 @@ class MapsController < ApplicationController
     lat = params[:lat].to_f
     lng = params[:lng].to_f
     @map = GMap.new("map_div")
-    @map.control_init(:large_map => true,:map_type => true)
+    @map.control_init(:large_map => true, :map_type => true)
     @map.set_map_type_init(GMapType::G_HYBRID_MAP)
     @map.center_zoom_init([lat,lng],params[:z].to_i)
     address = Geokit::Geocoders::GoogleGeocoder.do_reverse_geocode(Geokit::LatLng.new(lat,lng))
     full_address = address.full_address if address.respond_to? :full_address
     @map.overlay_init(GMarker.new([lat,lng],
                                   :title => full_address,
-                                  :info_window => "<h3>Address:</h3>#{full_address}"
+                                  :info_window => "<div class=\"gmls-result-wrapper\"><p>#{full_address}</p></div>"
                                   ))
     respond_to do |format|
       format.html
@@ -19,13 +19,28 @@ class MapsController < ApplicationController
   end
 
   def search
-    @map = GMap.new("map_div")
-    @map.control_init(:large_map => true,:map_type => true, :local_search => true, :local_search_options => "{suppressZoomToBounds : true, resultList: google.maps.LocalSearch.RESULT_LIST_INLINE, suppressInitialResultSelection : true, searchFormHint : 'Local Search powered by Google', linkTarget : GSearch.LINK_TARGET_BLANK}")
-    @map.set_map_type_init(GMapType::G_HYBRID_MAP)
-    @map.center_zoom_init([75.5,-42.56],4)
-    
     @server_id = params[:server]
     @hunt_id = params[:hunt]
+    @map = GMap.new("map")
+    @map.control_init(:large_map => true, :map_type => true, :local_search => true, :anchor => :bottom_left, :offset_width => 0, :offset_height => -30, :local_search_options => "{resultList : document.getElementById('results'), onGenerateMarkerHtmlCallback : extendMarker}")
+    @map.set_map_type_init(GMapType::G_HYBRID_MAP)
+    @map.center_zoom_init([44.501824,11.340463],11)
+    form = "<p><em>If you think this is the correct solution...</em><form action='http://apps.facebook.com#{Facebooker.facebook_path_prefix}/servers/#{@server_id}/treasure_hunts/#{@hunt_id}/answer?type=geoloc' method='post' target='_top'><input type='hidden' name='geoloc_lat' value='\"+ latlng.lat() +\"' /><input type='hidden' name='geoloc_long' value='\"+ latlng.lng() +\"' /><input type='submit' value='Submit it!' /></form>";
+    @map.record_init("function extendMarker(marker, html, result) {
+      // remove directions to .. from ..
+      gsdir = html.getElementsByClassName('gs-directions-to-from')[0];
+      html.removeChild(gsdir);
+      // get latlng
+      latlng = marker.getLatLng();
+      // create form
+      div = document.createElement('div');
+      div.innerHTML = \"#{form}\";
+      html.appendChild(div);
+      return html;}")
+    @map.event_init(@map, :click, "function(overlay, latlng) {
+    var myHtml = '<div class=\"gmls-result-wrapper\">Latitude: ' + latlng.lat() + '<br />Longitude: ' + latlng.lng() + \"#{form}</div>\";
+    map.openInfoWindow(latlng, myHtml); }")
+
     respond_to do |format|
       format.html
     end
